1cf9a1120a73c7c3fcc518e63e93e2b633ebcd79
DarkChan <buihohaidang@gmail.com>
2023-09-01 01:35:59 +0700

    steady

---
+++ Contact with SC.js
@@ -0,0 +1,14 @@
+const Web3 = require('web3');
+const web3 = new Web3('YOUR_PROVIDER_URL');
+
+const contractAddress = 'CONTRACT_ADDRESS';
+const contractABI = []; // ABI của smart contract
+
+const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+// Gửi Ether vào hợp đồng
+const amountToSend = web3.utils.toWei('1', 'ether');
+contractInstance.methods.sendPayment().send({ from: 'SENDER_ADDRESS', value: amountToSend });
+
+// Rút số tiền từ hợp đồng
+contractInstance.methods.withdraw().send({ from: 'OWNER_ADDRESS' });
--- storage/FundMe.sol
+++ storage/FundMe.sol
@@ -7,19 +7,24 @@ pragma solidity ^0.8.8;
 
 contract FundMe {
     uint256 public number;
+    uint256 public minimumUSD = 50;
 
     function fund() public payable {
         // payable using when you want to send any token
         // Want to be able to set a minimum fund amount of USD
         // 1. How do we send ETH to this contract?
         number = 5; // when reverting -> this will erase 5 in number and return gas from implement this number to 5
-        require(msg.value > 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 100000000000000000 -> 1 ETH
+        require(msg.value >= minimumUSD, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 100000000000000000 -> 1 ETH
         //'msg. sender': It is always the address of the account from where the function call came from.
         //'msg. value': The amount of Ether/Wei deposited or withdrawn by the msg. sender.
         // require = if, else = didn't send enough
         // what is reverting?
         // like if require fail, undo any action before, and send remaining gas back,
         // there is a variable number, and if we call the fund function and we didn't send enough value -> the number wont be update to 5
+
+        // how to convert ETH to USD -> oracle
+        // block chain can't call the api or http 
+        // chain link data feed
     }
 
     function getPrice() public {
